2b13fe80a77080e5173847d3aa929bca04f2755f
Ninjastahr <ihstahr@gmail.com>
2021-07-10 17:26:43 -0500

    Created index.ts based off of holotools-api

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import VideoHandler from './client/videos';
+import ChannelHandler from './client/channels';
+import CommentHandler from './client/comments';
+import { Settings } from './types';
+
+class Client {
+	/**
+	 * @private
+	 * @internal
+	 */
+	private url: string;
+
+	public videos: VideoHandler;
+
+	public channels: ChannelHandler;
+
+	public comments: CommentHandler;
+
+	constructor(settings: Settings = {}) {
+		this.url = settings.url ?? 'https://api.holotools.app/v1';
+		this.videos = new VideoHandler(settings);
+		this.channels = new ChannelHandler(settings);
+		this.comments = new CommentHandler(settings);
+	}
+}
+
+export {
+	Client, VideoHandler, ChannelHandler, CommentHandler,
+};
+
+// @ts-expect-error Redefine error
+export = Client;
