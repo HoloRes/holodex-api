6db4dc064123f6e4e3b3a3b5301e726bc1ee259c
GoldElysium <goldelysium@gmail.com>
2021-07-11 17:39:33 +0200

    Clean up map function

--- src/client/videos.ts
+++ src/client/videos.ts
@@ -46,105 +46,35 @@ class VideoHandler {
 	// eslint-disable-next-line class-methods-use-this,@typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any
 	mapVideos(data: any): VideoFull[] {
 		return data.map((video: APIVideoFull) => {
-			const clips: VideoWithChannel[]|undefined = video.clips?.map((clip: APIVideoWithChannel) => ({
-				id: clip.id,
-				title: clip.title,
-				type: clip.type,
-				topicId: clip.topic_id ?? undefined,
-				publishedAt: clip.published_at ? new Date(clip.published_at) : undefined,
-				availableAt: clip.available_at ? new Date(clip.available_at) : undefined,
-				duration: clip.duration,
-				status: clip.status,
-				startScheduled: clip.start_scheduled ? new Date(clip.start_scheduled) : undefined,
-				startActual: clip.start_actual ? new Date(clip.start_actual) : undefined,
-				endActual: clip.end_actual ? new Date(clip.end_actual) : undefined,
-				liveViewers: clip.live_viewers ?? undefined,
-				description: clip.description,
-				songcount: clip.songcount,
-				channelId: clip.channel_id,
+			const mapApiVideoWithChannelToVideoWithChannel = (videos: APIVideoWithChannel[]) => videos.map((vid) => ({
+				id: vid.id,
+				title: vid.title,
+				type: vid.type,
+				topicId: vid.topic_id ?? undefined,
+				publishedAt: vid.published_at ? new Date(vid.published_at) : undefined,
+				availableAt: vid.available_at ? new Date(vid.available_at) : undefined,
+				duration: vid.duration,
+				status: vid.status,
+				startScheduled: vid.start_scheduled ? new Date(vid.start_scheduled) : undefined,
+				startActual: vid.start_actual ? new Date(vid.start_actual) : undefined,
+				endActual: vid.end_actual ? new Date(vid.end_actual) : undefined,
+				liveViewers: vid.live_viewers ?? undefined,
+				description: vid.description,
+				songcount: vid.songcount,
+				channelId: vid.channel_id,
 				channel: {
-					id: clip.channel.id,
-					name: clip.channel.name,
-					englishName: clip.channel.english_name ?? undefined,
-					type: clip.channel.type,
-					photo: clip.channel.photo ?? undefined,
+					id: vid.channel.id,
+					name: vid.channel.name,
+					englishName: vid.channel.english_name ?? undefined,
+					type: vid.channel.type,
+					photo: vid.channel.photo ?? undefined,
 				},
-			}));
+			}))
 
-			const sources: VideoWithChannel[]|undefined = video.sources?.map((source: APIVideoWithChannel) => ({
-				id: source.id,
-				title: source.title,
-				type: source.type,
-				topicId: source.topic_id ?? undefined,
-				publishedAt: source.published_at ? new Date(source.published_at) : undefined,
-				availableAt: source.available_at ? new Date(source.available_at) : undefined,
-				duration: source.duration,
-				status: source.status,
-				startScheduled: source.start_scheduled ? new Date(source.start_scheduled) : undefined,
-				startActual: source.start_actual ? new Date(source.start_actual) : undefined,
-				endActual: source.end_actual ? new Date(source.end_actual) : undefined,
-				liveViewers: source.live_viewers ?? undefined,
-				description: source.description,
-				songcount: source.songcount,
-				channelId: source.channel_id,
-				channel: {
-					id: source.channel.id,
-					name: source.channel.name,
-					englishName: source.channel.english_name ?? undefined,
-					type: source.channel.type,
-					photo: source.channel.photo ?? undefined,
-				},
-			}));
-
-			const refers: VideoWithChannel[]|undefined = video.refers?.map((refer: APIVideoWithChannel) => ({
-				id: refer.id,
-				title: refer.title,
-				type: refer.type,
-				topicId: refer.topic_id ?? undefined,
-				publishedAt: refer.published_at ? new Date(refer.published_at) : undefined,
-				availableAt: refer.available_at ? new Date(refer.available_at) : undefined,
-				duration: refer.duration,
-				status: refer.status,
-				startScheduled: refer.start_scheduled ? new Date(refer.start_scheduled) : undefined,
-				startActual: refer.start_actual ? new Date(refer.start_actual) : undefined,
-				endActual: refer.end_actual ? new Date(refer.end_actual) : undefined,
-				liveViewers: refer.live_viewers ?? undefined,
-				description: refer.description,
-				songcount: refer.songcount,
-				channelId: refer.channel_id,
-				channel: {
-					id: refer.channel.id,
-					name: refer.channel.name,
-					englishName: refer.channel.english_name ?? undefined,
-					type: refer.channel.type,
-					photo: refer.channel.photo ?? undefined,
-				},
-			}));
-
-			const simulcasts: VideoWithChannel[]|undefined = video.simulcasts?.map((simulcast: APIVideoWithChannel) => ({
-				id: simulcast.id,
-				title: simulcast.title,
-				type: simulcast.type,
-				topicId: simulcast.topic_id ?? undefined,
-				publishedAt: simulcast.published_at ? new Date(simulcast.published_at) : undefined,
-				availableAt: simulcast.available_at ? new Date(simulcast.available_at) : undefined,
-				duration: simulcast.duration,
-				status: simulcast.status,
-				startScheduled: simulcast.start_scheduled ? new Date(simulcast.start_scheduled) : undefined,
-				startActual: simulcast.start_actual ? new Date(simulcast.start_actual) : undefined,
-				endActual: simulcast.end_actual ? new Date(simulcast.end_actual) : undefined,
-				liveViewers: simulcast.live_viewers ?? undefined,
-				description: simulcast.description,
-				songcount: simulcast.songcount,
-				channelId: simulcast.channel_id,
-				channel: {
-					id: simulcast.channel.id,
-					name: simulcast.channel.name,
-					englishName: simulcast.channel.english_name ?? undefined,
-					type: simulcast.channel.type,
-					photo: simulcast.channel.photo ?? undefined,
-				},
-			}));
+			const clips: VideoWithChannel[]|undefined = video.clips ? mapApiVideoWithChannelToVideoWithChannel(video.clips) : undefined;
+			const sources: VideoWithChannel[]|undefined = video.sources ? mapApiVideoWithChannelToVideoWithChannel(video.sources) : undefined;
+			const refers: VideoWithChannel[]|undefined = video.refers ? mapApiVideoWithChannelToVideoWithChannel(video.refers) : undefined;
+			const simulcasts: VideoWithChannel[]|undefined = video.simulcasts ? mapApiVideoWithChannelToVideoWithChannel(video.simulcasts) : undefined;
 
 			const mentions: Mention[]|undefined = video.mentions?.map((channel: APIMentions) => ({
 				id: channel.id,
